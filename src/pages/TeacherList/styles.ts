12ced3af3d80fd8052ba2ddb168c12ad2850261d
Mateus Gonçalves Rizzo <mateusrizzo13@gmail.com>
2020-08-07 18:15:56 -0300

    search filters added

--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -1,4 +1,6 @@
 import {StyleSheet} from 'react-native';
+import { Poppins_400Regular } from '@expo-google-fonts/poppins';
+import { Archivo_700Bold } from '@expo-google-fonts/archivo';
 
 const styles = StyleSheet.create({
     container: {
@@ -7,7 +9,44 @@ const styles = StyleSheet.create({
     },
     teacherList: {
         marginTop: -40,
-    }
+    },
+    searchForm: {
+        marginBottom: 24,
+    },
+    label: {
+        color: '#d4c2ff',
+        fontFamily: 'Poppins_400Regular',
+    },
+    inputGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    inputBlock: {
+        width: '48%'
+    },
+    input: {
+        height: 54,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        justifyContent: 'center',
+        paddingHorizontal: 16,
+        marginTop: 4,
+        marginBottom: 10,
+    },
+    submitButton: {
+        backgroundColor: '#04d363',
+        height: 56,
+        borderRadius: 8,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    submitButtonText: {
+        color: '#fff',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16,
+        marginLeft: 16,
+    },
 })
 
 export default styles;
